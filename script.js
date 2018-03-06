577d9e941578fec544f41983250b9e0d0b1dfe50
bhickey77 <williamhickey@Williams-MacBook-Pro.local>
2018-03-05 23:07:33 -0600

    work experience example styling'

---
+++ script.js
@@ -0,0 +1,17 @@
+function hideExamples() {
+  var x = document.getElementById("example1");
+  if (x.style.display === "none") {
+      x.style.display = "block";
+  } else {
+      x.style.display = "none";
+  }
+}
+
+function hideExamples2() {
+  var x = document.getElementById("example2");
+  if (x.style.display === "none") {
+      x.style.display = "block";
+  } else {
+      x.style.display = "none";
+  }
+}
