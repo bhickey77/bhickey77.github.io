dca477b93afc185dbff64b4864579c938222f73a
bhickey77 <williamhickey@Williams-MacBook-Pro.local>
2018-03-06 21:17:41 -0600

    add glyphicon arrow and animation

--- script.js
+++ script.js
@@ -15,3 +15,22 @@ function hideExamples2() {
       x.style.display = "none";
   }
 }
+
+$(document).ready(function() {
+  $( ".button" ).click( function() {
+      var self   = $(this),
+      index  = self.index(),
+      text   = self.text();
+      if(index == 4) {
+        index = 0;
+      } else {
+        index = 1;
+      }
+    console.log($(".glyphicon-chevron-down").eq(index).css('transform'));
+    if ($(".glyphicon-chevron-down").eq(index).css('transform') == 'none') {
+      $(".glyphicon-chevron-down").eq(index).css({'transform': 'rotate(-180deg)'});
+    } else {
+      $(".glyphicon-chevron-down").eq(index).css({'transform': ''});
+    };
+  });
+});
